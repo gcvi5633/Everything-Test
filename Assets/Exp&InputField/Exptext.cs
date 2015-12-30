845838ab4ed6845ee0745b7c02641e5ad838e896
gcvi5633 <gcvi5633@gmail.com>
2015-12-31 02:55:13 +0800

    新增了 Exp 的升級，經驗值倍率成長， InputField 的試用，與 LoadingScreen 過場讀取的場景範例

---
+++ Assets/Exp&InputField/Exptext.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Exptext : MonoBehaviour {
+
+    public Text expTxt;
+    public Text levelTxt;
+    public Text expFloor;
+    public InputField inputExp;
+
+    private float exp;//玩家現有經驗值
+    private float maxExp;//所需經驗值
+    private float maxExpGrow;//所需經驗值成長倍率
+    private int floor;//經驗值倍率階層
+    private int level;//等級
+    // Use this for initialization
+    void Start () {
+        exp = 0;
+        maxExp = 100;
+        maxExpGrow = 100;
+        floor = 1;
+        level = 1;
+        setText();
+    }
+
+	// Update is called once per frame
+	void Update () {
+        //若是玩家經驗值大於所需經驗值，玩家經驗值就會減去上一次的所需經驗值
+        //然後所需經驗值會成長，等級會增加
+        if(exp >= maxExp){
+            exp = exp - maxExp;
+            maxExp = maxExp + maxExpGrow;
+            level++;
+            //若是等級能夠被一個等級階層，例如下面的10等，階層就會增加並且增加經驗值倍率
+            if((level % 10) == 0){
+                floor++;
+                maxExpGrow = maxExpGrow * floor;
+            }
+        }
+        setText();
+    }
+
+    public void expPlus(int minu_){
+        minu_ = int.Parse(inputExp.text);//使用 int.Parse() 來將 String 轉換為 Int32
+        if (minu_ < 0) return; //經驗值輸入小於 0 則不會有反應
+        exp = exp + minu_;
+    }
+
+    void setText(){
+        expTxt.text = exp.ToString("F" + 0) + "/" + maxExp.ToString("F" + 0);
+        levelTxt.text = "LV: " + level.ToString();
+        expFloor.text = "第 " + floor.ToString() + " 階經驗值倍率";
+    }
+}
---
+++ Assets/LoadingScreen/loadingScreen.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loadingScreen : MonoBehaviour {
+
+    public string levelToLoad;
+
+    public GameObject background;
+    public GameObject text;
+    public GameObject progressBar;
+
+    //設一個進度讀取值，這個的值在 0 到 1 之間
+    private int loadProgress = 0;
+	// Use this for initialization
+	void Start () {
+        background.SetActive(false);
+        text.SetActive(false);
+        progressBar.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            //按下 space 就會開始執行 DisplayLoadingScreen 這個程式
+            StartCoroutine(DisplayLoadingScreen(levelToLoad));
+        }
+	}
+
+    IEnumerator DisplayLoadingScreen(string level)
+    {
+        //將進度讀取介面打開
+        background.SetActive(true);
+        text.SetActive(true);
+        progressBar.SetActive(true);
+
+        /*progressBar.transform.localScale = new Vector3(loadProgress, progressBar.transform.localScale.y, progressBar.transform.localScale.z);
+
+        text.GetComponent<GUIText>().text = "Loading... " + loadProgress + "%";*/
+
+        //設置一個異步讀取的對象   進行異步讀取場景 level 就是要讀取的場景
+        AsyncOperation async = Application.LoadLevelAsync(level);
+
+        while (!async.isDone)
+        {
+            //將讀取的進度值 *100 使之最大可到 100 ，之後轉換為整數
+            loadProgress = (int)(async.progress*100);
+
+            //讀取條表現方式控制碼
+            progressBar.transform.localScale = new Vector3(async.progress, progressBar.transform.localScale.y, progressBar.transform.localScale.z);
+
+            //Unity 5 之後要使用 GUIText 就要是用下面的方式來抓取 GUIText 裡的參數
+            text.GetComponent<GUIText>().text = "Loading... " + loadProgress + "%";
+
+            //回傳 null ，此方法會回到上層的 StartCoroutine ，重新呼叫一次
+            yield return null;
+        }
+    }
+}
